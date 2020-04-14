a86c083826a43592bb7f0bfa0673ec511054b0fa
v04jais <vjaiswal604@gmail.com>
2020-04-14 23:02:13 +0530

    adding dead lock

--- JavaLearning/src/synchronization/DeadLock.java
+++ JavaLearning/src/synchronization/DeadLock.java
@@ -3,7 +3,46 @@ package synchronization;
 public class DeadLock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		final String resource1="VISHAL";
+		final String resource2="ANKITA";
+		
+	Thread t1=new Thread() {
+			public void run() {
+				synchronized(resource1) {
+					
+					System.out.println("Thread 1 Locked Resoucre 1");
+					 try { Thread.sleep(1000);} catch (Exception e) {}  
+					synchronized(resource2) {
+						
+						System.out.println("Thread 1 Locked Resource2");
+						
+					}
+					
+				}
+				
+			}
+		};
+		
+	Thread t2=	new Thread() {
+			public void run() {
+				synchronized(resource2) {
+					
+					System.out.println("Thread 2 : Locked Resource 2");
+					 try { Thread.sleep(1000);} catch (Exception e) {}  
+					synchronized(resource1) {
+						
+						System.out.println("Thread 2: Locked Resource 1");
+						
+					}
+					
+				}
+				
+			}
+		};
+		
+		t1.start();
+		t2.start();
 
 	}
 
